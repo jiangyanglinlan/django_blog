dd2668b94649857f4453c7ec04b15ad760a35936
jiangyanglinlan <1815212328@qq.com>
2019-05-19 21:49:36 +0800

    拆分 settings

--- django_blog/django_blog/wsgi.py
+++ django_blog/django_blog/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings')
+profile = os.environ.get('BLOG_PROFILE', 'develop')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings.{}'.format(profile))
 
 application = get_wsgi_application()
