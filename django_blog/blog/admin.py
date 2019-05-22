598c603da5ba49cd702d2462b4962c4661c517c1
jiangyanglinlan <1815212328@qq.com>
2019-05-22 23:14:52 +0800

    post admin

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-from .models import Post, Category, Tag
+from .models import (
+    Post,
+    Category,
+    Tag,
+)
 
 
 @admin.register(Category)
@@ -31,7 +37,48 @@ class TagAdmin(admin.ModelAdmin):
         'owner',
         'created_time',
     )
+    fields = (
+        'name',
+        'status',
+    )
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'category',
+        'status',
+        'created_time',
+        'operator',
+    ]
+    list_display_links = []
+
+    list_filter = ['category', ]
+    search_fields = ['title', 'category__name']
+
+    # 动作相关的配置在顶部和底部均显示
+    actions_on_top = True
+    actions_on_bottom = True
+
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag'
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id, ))
+        )
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
